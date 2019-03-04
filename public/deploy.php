5c5fbab7bfa87a2ad774b396b579650d19caf1d9
Alex Lucena <aluckyar@gmail.com>
2019-03-04 13:58:23 +0100

    Fix #3 Security and functionality improvements in auto deploy

--- public/deploy.php
+++ public/deploy.php
@@ -50,39 +50,41 @@ if (!$allowed) {
     header('HTTP/1.1 403 Forbidden');
     echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
     echo "</pre>\n</body>\n</html>";
-    exit;
+    $log = "IP NO ACCESS: " . $ip . "\n";
+} else {
+    flush();
+
+    // Actually run the update
+
+    $commands = array(
+        'echo $PWD',
+        'whoami',
+        'git pull',
+        'git status',
+    );
+
+    $output = "\n";
+
+    $log = "####### " . date('Y-m-d H:i:s') . " #######\n";
+
+    foreach ($commands AS $command) {
+        // Run it
+        $tmp = shell_exec("$command 2>&1");
+        // Output
+        $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+        $output .= htmlentities(trim($tmp)) . "\n";
+
+        $log .= "\$ $command\n" . trim($tmp) . "\n";
+    }
+
+    $log .= "\n";
+    
+    echo $output;
 }
-
-flush();
-
-// Actually run the update
-
-$commands = array(
-    'echo $PWD',
-    'whoami',
-    'git pull',
-    'git status',
-);
-
-$output = "\n";
-
-$log = "####### " . date('Y-m-d H:i:s') . " #######\n";
-
-foreach ($commands AS $command) {
-    // Run it
-    $tmp = shell_exec("$command 2>&1");
-    // Output
-    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-    $output .= htmlentities(trim($tmp)) . "\n";
-
-    $log .= "\$ $command\n" . trim($tmp) . "\n";
-}
-
-$log .= "\n";
 
 file_put_contents('deploy-log.txt', $log, FILE_APPEND);
 
-echo $output;
+
 ?>
 </pre>
 </body>
